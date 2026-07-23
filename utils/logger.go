3617574fbef0b8a6cceaba66b94e9b1af052a143
iixplanet <107902908+iixplanet@users.noreply.github.com>
2026-07-23 15:47:39 +0700

    Update logger.go

--- utils/logger.go
+++ utils/logger.go
@@ -10,6 +10,11 @@ import (
 
 var Logger *log.Logger
 
+// ClearLog mengosongkan isi file log (truncate)
+func ClearLog(logFile string) {
+	os.WriteFile(logFile, []byte(""), 0644)
+}
+
 func InitLogger(logFile string) {
 	os.MkdirAll(filepath.Dir(logFile), 0755)
 	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -22,24 +27,18 @@ func InitLogger(logFile string) {
 
 func LogInfo(format string, v ...interface{}) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Println("\033[1;32m[INFO]\033[0m", msg) // Green text for terminal
-	if Logger != nil {
-		Logger.Println("INFO:", msg)
-	}
+	fmt.Println("\033[1;32m[INFO]\033[0m", msg)
+	if Logger != nil { Logger.Println("INFO:", msg) }
 }
 
 func LogWarn(format string, v ...interface{}) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Println("\033[1;33m[WARN]\033[0m", msg) // Yellow text for terminal
-	if Logger != nil {
-		Logger.Println("WARN:", msg)
-	}
+	fmt.Println("\033[1;33m[WARN]\033[0m", msg)
+	if Logger != nil { Logger.Println("WARN:", msg) }
 }
 
 func LogError(format string, v ...interface{}) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Println("\033[1;31m[ERROR]\033[0m", msg) // Red text for terminal
-	if Logger != nil {
-		Logger.Println("ERROR:", msg)
-	}
+	fmt.Println("\033[1;31m[ERROR]\033[0m", msg)
+	if Logger != nil { Logger.Println("ERROR:", msg) }
 }
