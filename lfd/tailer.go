d64a601bf44a9bf6ddf37c5ac2b4fef1133bc83c
iixplanet <107902908+iixplanet@users.noreply.github.com>
2026-07-25 12:14:11 +0700

    Update tailer.go

--- lfd/tailer.go
+++ lfd/tailer.go
@@ -165,12 +165,6 @@ func parseLogLine(line, service string, maxLimit int) {
 					continue 
 				}
 				
-				// Pisahkan log antara serangan Web (WAF) dan serangan Bruteforce Login
-				if service == "MODSEC" {
-					utils.LogWarn("WAF ALARM: Target %s triggered Web Application Firewall rule.", cleanIP)
-				} else {
-					utils.LogWarn("AUTH ALARM: Target %s failed %s authentication.", cleanIP, service)
-				}
 				
 				AddStrike(cleanIP, service, maxLimit)
 				break
