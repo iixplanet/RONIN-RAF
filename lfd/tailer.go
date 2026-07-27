8d35c74ace11b09c5136143169d0c3da5d1cb3d4
iixplanet <107902908+iixplanet@users.noreply.github.com>
2026-07-27 20:36:36 +0700

    Update tailer.go

--- lfd/tailer.go
+++ lfd/tailer.go
@@ -97,6 +97,7 @@ var ThreatSignatures = map[string][]*regexp.Regexp{
 		regexp.MustCompile(`(?s)-+[a-zA-Z0-9]+-+A-+\s*\[[^\]]+\]\s+\S+\s+([a-fA-F0-9\.:]+).*?-+[a-zA-Z0-9]+-+H-+.*?(?:ModSecurity|Message).*?Access denied`),
 
 		regexp.MustCompile(`(?i)ModSecurity(?:\]|:).*?Access denied with code 403.*?\[msg\s+"?\s*((?:[0-9]{1,3}\.){3}[0-9]{1,3})`),
+		regexp.MustCompile(`(?s)A--\s*\[[^\]]+\]\s+\S+\s+([a-fA-F0-9\.:]+).*?-H--\s*(?:Message|ModSecurity).*?Access denied`),
 
 		// 4. LITESPEED NATIVE ERROR LOG STRICT MODE
 		// Contoh log: [140.83.85.172:59721] [ModSecurity] Access denied with code 403
